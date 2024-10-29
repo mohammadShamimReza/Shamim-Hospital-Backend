74e818775a41f688927f3a73466c02a7dc51d656
Md Shamim Reza <mohammadshamimreza23393@gmail.com>
2024-10-29 14:58:14 +0600

    fix service

---
+++ prisma/migrations/20241029085800_update/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `isEmergency` on the `service` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "service" DROP COLUMN "isEmergency";
--- src/app/modules/Service/Service.validation.ts
+++ src/app/modules/Service/Service.validation.ts
@@ -2,10 +2,33 @@ import { z } from 'zod';
 
 const createService = z.object({
   body: z.object({
-    serviceName: z.string({ required_error: 'Service name is required' }),
-    description: z.string({ required_error: 'Description is required' }),
-    duration: z.number().int({ message: 'Duration is required' }),
-    price: z.number({ required_error: 'Price is required' }),
+    serviceName: z.string({
+      required_error: 'Service name is required',
+    }),
+    description: z.string({
+      required_error: 'Description is required',
+    }),
+    duration: z.number().int({
+      message: 'Duration is required and must be an integer',
+    }),
+    price: z.number({
+      required_error: 'Price is required',
+    }),
+    serviceType: z
+      .string({
+        required_error: 'Service type is required',
+      })
+      .refine(val => ['Consultation', 'Surgery', 'Therapy'].includes(val), {
+        message:
+          'Service type must be one of "Consultation", "Surgery", or "Therapy"',
+      }),
+    bodyPart: z.string({
+      required_error: 'Body part is required',
+    }),
+    specialty: z.string({
+      required_error: 'Specialty is required',
+    }),
+    maxAppointments: z.number().int().optional(),
     departmentId: z.number().int().optional(),
   }),
 });
@@ -16,6 +39,10 @@ const updateService = z.object({
     description: z.string().optional(),
     duration: z.number().int().optional(),
     price: z.number().optional(),
+    serviceType: z.string().optional(),
+    bodyPart: z.string().optional(),
+    specialty: z.string().optional(),
+    maxAppointments: z.number().int().optional(),
     departmentId: z.number().int().optional(),
   }),
 });

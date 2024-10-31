2a6a774dd55b29740bc9068d293aab0a9400adb8
Md Shamim Reza <mohammadshamimreza23393@gmail.com>
2024-10-31 14:24:46 +0600

    admin functionality done

--- src/app/modules/admin/Admin.routes.ts
+++ src/app/modules/admin/Admin.routes.ts
@@ -19,7 +19,7 @@ router.post(
 
 router.patch(
   '/:id',
-  auth(ENUM_USER_ROLE.SUPER_ADMIN),
+  // auth(ENUM_USER_ROLE.SUPER_ADMIN),
   validateRequest(AdminValidation.updateAdmin),
   AdminController.updateAdmin,
 );
--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -9,12 +9,17 @@ import { MedicationRoutes } from "../modules/medication/Medicaiton.routes";
 import { StaffRoutes } from "../modules/staff/Staff.routes";
 import { UserRoutes } from "../modules/user/User.routes";
 import { ServiceRoutes } from "../modules/Service/Service.routes";
+import { AdminRoutes } from "../modules/admin/Admin.routes";
 
 
 const router = express.Router();
 
 const moduleRoutes = [
   { path: '/auth', routes: authRoutes },
+  {
+    path: '/admin',
+    routes: AdminRoutes,
+  },
   {
     path: '/doctor',
     routes: DoctorRoutes,
